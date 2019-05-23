0f4bd981371a3780aceb6ae763ba377d3c6e16ea
fralps <francois.loupias@orange.fr>
2019-05-23 10:46:00 +0200

    Table OK

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
+
+Task.destroy_all
+Category.destroy_all
+Email.destroy_all
+
 3.times do
   my_category = Category.create(title: Faker::Book.genre)
   3.times do
@@ -16,4 +21,10 @@ require 'faker'
     my_task.category = my_category
     my_task.save
   end
+end
+
+5.times do
+	my_email = Email.create(object: Faker::Lorem.sentence(4),
+													body: Faker::Lorem.paragraph(2))
+	puts "Email added"
 end
