1411ecb17370f03ce656bd66a9c76836ce861683
fralps <francois.loupias@orange.fr>
2019-05-23 11:10:30 +0200

    view OK, beginning AJAX

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,4 +2,28 @@ class EmailsController < ApplicationController
   def index
   	@emails = Email.all
   end
+
+  def create
+  	@email = Email.new(object: Faker::Lorem.sentence(4),
+  												body: Faker::Lorem.paragraph)
+
+  	if @email.save
+
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js { }
+      end
+
+      flash[:notice] = "You received an email"
+    else
+      redirect_to root_path
+      flash[:notice] = "Something went wrong"
+    end
+  end
+
+  def destroy  	
+  end
+
+  def show  	
+  end
 end
